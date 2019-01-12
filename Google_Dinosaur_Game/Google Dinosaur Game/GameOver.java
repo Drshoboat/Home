662ed6302b5cd88b7b965ea40ebd95776387ccff
Drshoboat <drshoboat@gmail.com>
2019-01-11 20:36:42 -0600

    idk

--- Google_Dinosaur_Game/Google Dinosaur Game/GameOver.java
+++ Google_Dinosaur_Game/Google Dinosaur Game/GameOver.java
@@ -2,8 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class GameOver here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class GameOver extends World
@@ -11,28 +11,19 @@ public class GameOver extends World
 
     /**
      * Constructor for objects of class GameOver.
-     * 
+     *
      */
-<<<<<<< HEAD
-    public void act() 
-    {
-        
-    }  
-    
-
-=======
     public GameOver()
-    {    
+    {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
+        super(800, 600, 1);
         act();
     }
-    
+
     public void act() {
         if(Greenfoot.isKeyDown("space")) {
             Greenfoot.delay(5);
             Greenfoot.setWorld(new MyWorld());
         }
     }
->>>>>>> dbdf678efb896de9b938f44cf850a0a0edfdcd9e
 }
--- Google_Dinosaur_Game/Google Dinosaur Game/MyWorld.java
+++ Google_Dinosaur_Game/Google Dinosaur Game/MyWorld.java
@@ -4,23 +4,23 @@ import java.lang.Math;
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class MyWorld extends World
 {
     /**
      * Constructor for objects of class MyWorld.
-     * 
+     *
      */
-    
+
     public MyWorld()
-    {    
+    {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1, false); 
-        
+        super(800, 600, 1, false);
+
         prepare();
-        
+
     }
 
     /**
@@ -35,7 +35,7 @@ public class MyWorld extends World
         addObject(new Cactii(), 850, getHeight()/2);
         addObject(new Cactii(), 900, getHeight()/2);
     }
-    
+
     public void act(){
         if(getObjects(Cactii.class).get(0).getX() < -100) {
             removeObjects(getObjects(Cactii.class));
@@ -44,6 +44,6 @@ public class MyWorld extends World
             }
         }
     }
-    
+
 
 }
