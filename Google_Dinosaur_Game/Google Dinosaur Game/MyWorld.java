4e4e0b18cda805b03d8cc4cc3c0d1c1d1457a110
Drshoboat <drshoboat@gmail.com>
2019-01-27 21:42:42 -0600

    Changed background, added clouds, added floor, added dirt

--- Google_Dinosaur_Game/Google Dinosaur Game/MyWorld.java
+++ Google_Dinosaur_Game/Google Dinosaur Game/MyWorld.java
@@ -18,6 +18,9 @@ public class MyWorld extends World
     {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1, false);
+        GreenfootImage bob = new GreenfootImage("", 45,new Color(150, 148, 148), new Color(150, 148, 148));
+        
+        setBackground(bob);
 
         prepare();
 
@@ -30,20 +33,40 @@ public class MyWorld extends World
     private void prepare()
     {
         MemeDinoBoi memeDinoBoi = new MemeDinoBoi();
-        CurrentScore score = new CurrentScore();
-        //Cactii cactii = new Cactii();
-        addObject(score, getWidth() - 100, 200);
+        //CurrentScore score = new CurrentScore();
+        Cactii cactii = new Cactii();
+        //addObject(score, getWidth() - 100, 200);
+        addObject(new Cloud(), 800, 50);
+        addObject(new floor(), 400,285);
         addObject(memeDinoBoi, 99, getHeight()/2);
+        
         addObject(new Cactii(), 850, getHeight()/2);
         addObject(new Cactii(), 900, getHeight()/2);
-        
+        addObject(new Dirt(), 750, 500);
+        addObject(new Dirt(), 2000, 475);
     }
 
     public void act(){
         if(getObjects(Cactii.class).get(0).getX() < -100) {
             removeObjects(getObjects(Cactii.class));
             for(int i = 0; i < (int)(Math.random() * 2) + 2; i++){
-                addObject(new Cactii(), (int)(Math.random()* 300) + 850, getHeight()/2);
+                addObject(new Cactii(), (int)(Math.random()* 110) + 850, getHeight()/2);
+            }
+        }
+        
+        addObject(new Dirt(), (int)(Math.random()* 500) + 800, (int)(Math.random() * 600) + 380);
+        if(getObjects(Dirt.class).get(0).getX() < -1000) {
+            //removeObjects(getObjects(Dirt.class));
+            for(int i = 0; i < (int)(Math.random() * 6); i++){
+                addObject(new Dirt(), (int)(Math.random()* 600) + 800, (int)(Math.random() * 800) + 380);
+            }
+            
+        }
+        
+         if(getObjects(Cloud.class).get(0).getX() < -50) {
+            removeObjects(getObjects(Cloud.class));
+            for(int i = 0; i < (int)(Math.random() * 5) + 2; i++){
+                addObject(new Cloud(), (int)(Math.random()* 110) + 1000, (int)(Math.random() * 75) + 50);
             }
         }
     }
